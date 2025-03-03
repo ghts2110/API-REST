e199487245acea985ec23c91e9c10c131a7a63a5
ghts2110 <gabrieltorres2110@gmail.com>
2025-03-02 22:20:58 -0300

    description controller

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import com.example.domain.model.User;
 import com.example.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -18,12 +19,14 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a user by ID", description = "Retrieve a specific user based on its ID")
     public ResponseEntity<User> findById(@PathVariable Long id){
         var user = userService.findUserById(id);
         return ResponseEntity.ok(user);
     }
 
     @PostMapping
+    @Operation(summary = "Create a new user", description = "Create a new user and return the created user's data")
     public ResponseEntity<User> create(@RequestBody User userToCreate){
         var userCreated = userService.createUser(userToCreate);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
@@ -33,4 +36,5 @@ public class UserController {
 
         return ResponseEntity.created(location).body(userCreated);
     }
+
 }
--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -6,4 +6,5 @@ public interface UserService
 {
     public User findUserById(Long id);
     public User createUser(User user);
+    public User updateUser(Long id, User user);
 }
